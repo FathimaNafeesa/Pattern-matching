933f3b93c7a9bad5d655e73939ee984602a6eb93
fathima <fathimanafeesavy@gmail.com>
2020-03-27 12:21:03 +0530

    [fathima]pattern matching of mail id abc.xyz@bridgelabz.co.in

--- mail.sh
+++ mail.sh
@@ -6,8 +6,8 @@ p1="^([a-z]{3})+"
 p4="(\.[a-z0-9]*)"
 p2=(@bridgelabz)
 p3="\.(com|in)$"
-
-if [[ $mail =~ $p1$p4$p2$p3 ]]
+p5="(\.co)*"
+if [[ $mail =~ $p1$p4$p2$p5$p3 ]]
 then
         echo "valid"
 else
